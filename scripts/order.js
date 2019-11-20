eb141ecbff72ba6d0676e78b61ac8ddbb10cafac
ultraluxe <ultraluxearp@users.noreply.github.com>
2019-11-21 00:33:32 +0500

    1.7

--- scripts/order.js
+++ scripts/order.js
@@ -6,7 +6,7 @@ var tasks = [];
 orderProduct.onclick = (event) => {
     event.preventDefault();
 
-    // var title = document.getElementById('task-title').value;
+    // get variables by getting the value from HTML IDs'
     var name = document.getElementById('name').value;
     var num = document.getElementById('num').value;
     var loc = document.getElementById('loc').value;
@@ -17,7 +17,7 @@ orderProduct.onclick = (event) => {
     var title = document.getElementById('title').textContent;
 
 
-
+// Validation whether the following variable fields contain something
     if (name !== '' &&  num !== '' && loc !== '' && zip !== '' && card !== '' && cvv !== '') {
 
         alert(name + ', ' + 'Thank you for choosing Ultraluxe Store!' + '\n' + '\n' + 'Product: ' + title + '\n' + 'Payment amount: ' + price + '\n' + 'Shipment address: ' + loc + ', ' + zip);
--- scripts/slideshow.js
+++ scripts/slideshow.js
@@ -9,7 +9,10 @@ function showSlides() {
     slides[i].style.display = "none";
   }
   slideIndex++;
+  // if n is more than the number of slides, then set slideNumber to 1
   if (slideIndex > slides.length) {slideIndex = 1}
+  // this is a loop, which goes through all the slides and adds inline styling to each of them:
+  // the following style attribute will be added to each .slide: style="display: block;"
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
